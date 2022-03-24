075c5d62926a93adb7b885f9cfb621791e12f133
Syed Irfan <irfan.syed@elektrobit.com>
2022-03-24 21:15:56 +0530

    refined code

--- js/card.js
+++ js/card.js
@@ -12,37 +12,27 @@ let colorsArray = [
 onSort(cardArray);
 function onShuffle() {
     for (var i = cardArray.length - 1; i > 0; i--) {
-
-        // Generate random number
         var j = Math.floor(Math.random() * (i + 1));
-
         var temp = cardArray[i];
         cardArray[i] = cardArray[j];
         cardArray[j] = temp;
     }
-    console.log(cardArray)
+
     renderCards(cardArray);
 }
 
 function renderCards(inputArr) {
     clearCards();
     for (let index = 0; index < inputArr.length; index++) {
-        const color = colorsArray[index];
         document.getElementById('cards').innerHTML += `<div class="item">
             <h1 class="numberText" >${inputArr[index]}</h1>
             </div>`;
     }
-    // if(screen.width <350 )  { 
-    //      document.getElementsByClassName('item').style.backgroundcolor = '#EFEFEF'
-    // }
     var grid = document.getElementsByClassName("item");
     var len = grid.length;
 
     for (var i = 0; i < len; i++) {
         const color = colorsArray[i];
-    //     if(screen.width <350 )  { 
-    //         grid[i].style.backgroundColor = '#EFEFEF'
-    // }
         grid[i].style.backgroundColor = color;
         grid[i].style.borderLeft = `20px solid ${color}`;
 
@@ -55,6 +45,5 @@ function clearCards() {
 
 function onSort() {
     cardArray = cardArray.sort();
-    console.log(cardArray)
     renderCards(cardArray);
 }
